c87df62e920356ab1d7f43a099cd27e369056c67
HongdaZ <zyqlxrhd@sina.cn>
2019-09-10 19:35:51 -0500

    A example .cpp function added

--- src/inplaceMS.cpp
+++ src/inplaceMS.cpp
@@ -1,19 +1,18 @@
 #include <R.h>
 #include <Rinternals.h>
 extern "C" {
-    void inplaceMS( SEXP A, SEXP B ) {
+    SEXP inplaceMS( SEXP A, SEXP B ) {
         int nra, nca;
         SEXP Adim;
         Adim = getAttrib( A, R_DimSymbol );
         nra = INTEGER( Adim )[ 0 ];
         nca = INTEGER( Adim )[ 1 ];
-        int i = 0, j = 0;
+        int i = 0, last = nra * nca;
         double *L = REAL( A ), *R = REAL( B );
-        for( i = 0; i != nra; ++ i ) {
-            for( j = 0; j != nca; ++ j  ) {
-                L[ j * nra + i ] += R[ j * nra + i ];
-            }
-        }
+        for( i = 0; i != last ; ++ i ) {
 
+                L[ i ] += R[ i ];
+        }
+        return A;
     }
 } // extern "C"
