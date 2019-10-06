c4695770e978112ceb82f477429a42e228a731be
HongdaZ <zyqlxrhd@sina.cn>
2019-10-05 21:15:24 -0500

    No change

---
+++ R/sumPatient.R
@@ -0,0 +1,26 @@
+sumPatient <- function( patient ) {
+    n <- length( patient )
+    sum_cross_y <- list( matrix( 0, nrow = 4, ncol = 4 ),
+                         matrix( 0, nrow = 4, ncol = 4 ),
+                         matrix( 0, nrow = 4, ncol = 4 ),
+                         matrix( 0, nrow = 4, ncol = 4 ) )
+    sum_y <- list( vector( "numeric", 4 ),
+                   vector( "numeric", 4 ),
+                   vector( "numeric", 4 ),
+                   vector( "numeric", 4 ) )
+    n_type <- vector( "integer", 4 )
+
+    for( i in 1 : n ) {
+        for( j in 1 : 4 ) {
+            sum_cross_y[[ j ]] <- sum_cross_y[[ j ]] +
+                patient[[ i ]][[ 1 ]][[ j ]]
+            sum_y[[ j ]] <-sum_y[[ j ]] +
+                patient[[ i ]][[ 2 ]][[ j ]]
+        }
+        n_type <- n_type +
+            patient[[ i ]][[ 3 ]]
+    }
+    list( sum_cross_y = sum_cross_y,
+          sum_y = sum_y,
+          n_type = n_type )
+}
