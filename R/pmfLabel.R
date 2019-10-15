1949c287210edb15b8f0181e48bd71ca83d12f0e
HongdaZ <zyqlxrhd@sina.cn>
2019-10-15 15:08:33 -0500

    pmfLabel

--- R/pmfLabel.R
+++ R/pmfLabel.R
@@ -1,5 +1,7 @@
-pmfLabel <- function( alpha_star, beta_star, gamma_star, neighbor_label,
-                      det_sigma, inv_sigma, sigma_mu, q_sigma, pw, hoi ) {
-    .Call( "pmfLabel", alpha_star, beta_star, gamma_star, neighbor_label,
-           det_sigma, inv_sigma, sigma_mu, q_sigma, pw, hoi )
+pmfLabel <- function( alpha_star, beta_star, gamma_star,
+                      det_sigma, inv_sigma, sigma_mu,
+                      q_sigma, pw, hoi, neighbor_label, prob ) {
+    .Call( "pmfLabel", alpha_star, beta_star, gamma_star,
+           det_sigma, inv_sigma, sigma_mu,
+           q_sigma, pw, hoi, neighbor_label, prob )
 }
