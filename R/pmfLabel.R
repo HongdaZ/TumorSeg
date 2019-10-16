679d32392b0e6fed5490208f85a1917bed4560db
HongdaZ <zyqlxrhd@sina.cn>
2019-10-15 19:39:45 -0500

    Add pmfLabel.cpp

--- R/pmfLabel.R
+++ R/pmfLabel.R
@@ -1,6 +1,6 @@
 pmfLabel <- function( alpha_star, beta_star, gamma_star,
                       det_sigma, inv_sigma, sigma_mu,
-                      q_sigma, pw, hoi, neighbor_label, prob ) {
+                      q_sigma, pw, hoi, neighbor_label, prob, p ) {
     .Call( "pmfLabel", alpha_star, beta_star, gamma_star,
            det_sigma, inv_sigma, sigma_mu,
            q_sigma, pw, hoi, neighbor_label, prob )
--- src/pmfLabel.cpp
+++ src/pmfLabel.cpp
@@ -21,4 +21,11 @@ extern "C" {
         res = inner( tmp, vec );
         return res;
     }
+    SEXP pmfLabel( SEXP alpha_star, SEXP beta_star, SEXP gamma_star,
+                   SEXP det_sigma, SEXP inv_sigma, SEXP sigma_mu,
+                   SEXP q_sigma, SEXP pw, SEXP hoi, SEXP neighbor_label,
+                   SEXP prob, SEXP p ) {
+
+        return R_NilValue;
+    }
 } // extern "C"
