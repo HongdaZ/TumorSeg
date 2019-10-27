5dafde4faf8dbedfc188db3a731ff08b7d004b89
HongdaZ <zyqlxrhd@sina.cn>
2019-10-26 22:24:11 -0500

    Finished

--- R/predictLabel.R
+++ R/predictLabel.R
@@ -40,17 +40,19 @@ predictLabel <- function( old, new, burnin = 1000, n = 10000, core = 12 ) {
                 new_alpha <- old_alpha
                 new_alpha[ k ] <- old_alpha[ k ] +
                     rnorm( 1, mean = 0, sd = sd )
-                proportion <- exp( ( old_alpha[ k ] - new_alpha[ k ] ) *
-                                       ( lambda_alpha[ k ] +
-                                             old[[ j ]]$n_type[ k ] ) -
-                                       ( logZ( old[[ j ]], old_alpha,
-                                               beta[ , j ], gamma[ j ], core ) -
-                                             logZ( old[[ j ]], new_alpha,
-                                                   beta[ , j ],
-                                                   gamma[ j ], core ) ) )
-                rho <- min( 1, proportion )
-                if( rbinom( 1, 1, rho ) == 1 ) {
-                    alpha[ k, j ] <- new_alpha[ k ]
+                if( new_alpha[ k ] > 0 ) {
+                    proportion <- exp( ( old_alpha[ k ] - new_alpha[ k ] ) *
+                                           ( lambda_alpha[ k ] +
+                                                 old[[ j ]]$n_type[ k ] ) -
+                                           ( logZ( old[[ j ]], old_alpha,
+                                                   beta[ , j ], gamma[ j ], core ) -
+                                                 logZ( old[[ j ]], new_alpha,
+                                                       beta[ , j ],
+                                                       gamma[ j ], core ) ) )
+                    rho <- min( 1, proportion )
+                    if( rbinom( 1, 1, rho ) == 1 ) {
+                        alpha[ k, j ] <- new_alpha[ k ]
+                    }
                 }
             }
             # update beta's
@@ -59,31 +61,35 @@ predictLabel <- function( old, new, burnin = 1000, n = 10000, core = 12 ) {
                 new_beta <- old_beta
                 new_beta[ k ] <- old_beta[ k ] +
                     rnorm( 1, mean = 0, sd = sd )
-                proportion <- exp( ( old_beta[ k ] - new_beta[ k ] ) *
-                                       ( lambda_beta[ k ] +
-                                             old[[ j ]]$beta_sum[ k ] ) -
-                                       ( logZ( old[[ j ]], alpha[ , j ],
-                                               old_beta, gamma[ j ], core ) -
-                                             logZ( old[[ j ]], alpha[ , j ],
-                                                   new_beta, gamma[ j ],
-                                                   core ) ) )
-                rho <- min( 1, proportion )
-                if( rbinom( 1, 1, rho ) == 1 ) {
-                    beta[ k, j ] <- new_beta[ k ]
+                if( new_beta[ k ] > 0 ) {
+                    proportion <- exp( ( old_beta[ k ] - new_beta[ k ] ) *
+                                           ( lambda_beta[ k ] +
+                                                 old[[ j ]]$beta_sum[ k ] ) -
+                                           ( logZ( old[[ j ]], alpha[ , j ],
+                                                   old_beta, gamma[ j ], core ) -
+                                                 logZ( old[[ j ]], alpha[ , j ],
+                                                       new_beta, gamma[ j ],
+                                                       core ) ) )
+                    rho <- min( 1, proportion )
+                    if( rbinom( 1, 1, rho ) == 1 ) {
+                        beta[ k, j ] <- new_beta[ k ]
+                    }
                 }
             }
             # update gamma
             old_gamma <- gamma[ j ]
             new_gamma <- old_gamma + rnorm( 1, 0, sd )
-            proportion <- exp( ( old_gamma - new_gamma ) *
-                                   ( lambda_gamma  + old[[ j ]]$gamma_sum ) -
-                                   ( logZ( old[[ j ]], alpha[ , j ],
-                                           beta[ , j ], old_gamma, core ) -
-                                         logZ( old[[ j ]], alpha[ , j ],
-                                               beta[ , j ], new_gamma, core ) ) )
-            rho <- min( 1, proportion )
-            if( rbinom( 1, 1, rho ) == 1 ) {
-                gamma[ j ] <- new_gamma
+            if( new_gamma > 0 ) {
+                proportion <- exp( ( old_gamma - new_gamma ) *
+                                       ( lambda_gamma  + old[[ j ]]$gamma_sum ) -
+                                       ( logZ( old[[ j ]], alpha[ , j ],
+                                               beta[ , j ], old_gamma, core ) -
+                                             logZ( old[[ j ]], alpha[ , j ],
+                                                   beta[ , j ], new_gamma, core ) ) )
+                rho <- min( 1, proportion )
+                if( rbinom( 1, 1, rho ) == 1 ) {
+                    gamma[ j ] <- new_gamma
+                }
             }
         }
         # get beta_sum and gamma_sum
@@ -97,20 +103,24 @@ predictLabel <- function( old, new, burnin = 1000, n = 10000, core = 12 ) {
             new_alpha <- old_alpha
             new_alpha[ k ] <- old_alpha[ k ] +
                 rnorm( 1, mean = 0, sd = sd )
-            proportion <- exp( ( old_alpha[ k ] - new_alpha[ k ] ) *
-                                   ( lambda_alpha[ k ] +
-                                         sum_new$n_type[ k ] ) -
-                                   ( logZS( new, old_alpha,
-                                           beta_star, gamma_star, new$beta_sum,
-                                           new$gamma_sum, core ) -
-                                         logZS( new, new_alpha,
-                                                beta_star,gamma_star,
-                                                new$beta_sum,
-                                                new$gamma_sum, core ) ) )
-            rho <- min( 1, proportion )
-            if( rbinom( 1, 1, rho ) == 1 ) {
-                alpha_star[ k ] <- new_alpha[ k ]
-                alpha_star_trace[ k, i + 1 ] <- new_alpha[ k ]
+            if( new_alpha[ k ] > 0 ) {
+                proportion <- exp( ( old_alpha[ k ] - new_alpha[ k ] ) *
+                                       ( lambda_alpha[ k ] +
+                                             sum_new$n_type[ k ] ) -
+                                       ( logZS( new, old_alpha,
+                                                beta_star, gamma_star, new$beta_sum,
+                                                new$gamma_sum, core ) -
+                                             logZS( new, new_alpha,
+                                                    beta_star,gamma_star,
+                                                    new$beta_sum,
+                                                    new$gamma_sum, core ) ) )
+                rho <- min( 1, proportion )
+                if( rbinom( 1, 1, rho ) == 1 ) {
+                    alpha_star[ k ] <- new_alpha[ k ]
+                    alpha_star_trace[ k, i + 1 ] <- new_alpha[ k ]
+                } else {
+                    alpha_star_trace[ k, i + 1 ] <- old_alpha[ k ]
+                }
             } else {
                 alpha_star_trace[ k, i + 1 ] <- old_alpha[ k ]
             }
@@ -121,21 +131,25 @@ predictLabel <- function( old, new, burnin = 1000, n = 10000, core = 12 ) {
             new_beta <- old_beta
             new_beta[ k ] <- old_beta[ k ] +
                 rnorm( 1, mean = 0, sd = sd )
-            proportion <- exp( ( old_beta[ k ] - new_beta[ k ] ) *
-                                   ( lambda_beta[ k ] +
-                                         new$beta_sum[ k ] ) -
-                                   ( logZS( new, alpha_star,
-                                           old_beta, gamma_star, new$beta_sum,
-                                           new$gamma_sum, core ) -
-                                         logZS( new, alpha_star,
-                                               new_beta, gamma_star,
-                                               new$beta_sum,
-                                               new$gamma_sum,
-                                               core ) ) )
-            rho <- min( 1, proportion )
-            if( rbinom( 1, 1, rho ) == 1 ) {
-                beta_star[ k ] <- new_beta[ k ]
-                beta_star_trace[ k, i + 1 ] <- new_beta[ k ]
+            if( new_beta[ k ] > 0 ) {
+                proportion <- exp( ( old_beta[ k ] - new_beta[ k ] ) *
+                                       ( lambda_beta[ k ] +
+                                             new$beta_sum[ k ] ) -
+                                       ( logZS( new, alpha_star,
+                                                old_beta, gamma_star, new$beta_sum,
+                                                new$gamma_sum, core ) -
+                                             logZS( new, alpha_star,
+                                                    new_beta, gamma_star,
+                                                    new$beta_sum,
+                                                    new$gamma_sum,
+                                                    core ) ) )
+                rho <- min( 1, proportion )
+                if( rbinom( 1, 1, rho ) == 1 ) {
+                    beta_star[ k ] <- new_beta[ k ]
+                    beta_star_trace[ k, i + 1 ] <- new_beta[ k ]
+                } else {
+                    beta_star_trace[ k, i + 1 ] <- old_beta[ k ]
+                }
             } else {
                 beta_star_trace[ k, i + 1 ] <- old_beta[ k ]
             }
@@ -143,24 +157,29 @@ predictLabel <- function( old, new, burnin = 1000, n = 10000, core = 12 ) {
         # update gamma_star
         old_gamma <- gamma_star
         new_gamma <- old_gamma + rnorm( 1, 0, sd )
-        proportion <- exp( ( old_gamma - new_gamma ) *
-                               ( lambda_gamma  + new$gamma_sum ) -
-                               ( logZS( new, alpha_star,
-                                       beta_star, old_gamma, new$beta_sum,
-                                       new$gamma_sum, core ) -
-                                     logZS( new, alpha_star,
-                                           beta_star, new_gamma, new$beta_sum,
-                                           new$gamma_sum, core ) ) )
-        rho <- min( 1, proportion )
-        if( rbinom( 1, 1, rho ) == 1 ) {
-            gamma_star <- new_gamma
-            gamma_star_trace[ i + 1 ] <- new_gamma
+        if( new_gamma > 0 ) {
+            proportion <- exp( ( old_gamma - new_gamma ) *
+                                   ( lambda_gamma  + new$gamma_sum ) -
+                                   ( logZS( new, alpha_star,
+                                            beta_star, old_gamma, new$beta_sum,
+                                            new$gamma_sum, core ) -
+                                         logZS( new, alpha_star,
+                                                beta_star, new_gamma, new$beta_sum,
+                                                new$gamma_sum, core ) ) )
+            rho <- min( 1, proportion )
+            if( rbinom( 1, 1, rho ) == 1 ) {
+                gamma_star <- new_gamma
+                gamma_star_trace[ i + 1 ] <- new_gamma
+            } else {
+                gamma_star_trace[ i + 1 ] <- old_gamma
+            }
         } else {
             gamma_star_trace[ i + 1 ] <- old_gamma
         }
+
         # update lamdba_alpha
         lambda_alpha <- rgamma( 4, shape =  s + a + 1,
-                                     rate = rowSums( alpha ) + b + alpha_star )
+                                rate = rowSums( alpha ) + b + alpha_star )
         # update lambda_beta
         lambda_beta <- rgamma( 3, shape =  s + a + 1,
                                rate = rowSums( beta ) + b + beta_star )
@@ -186,28 +205,50 @@ predictLabel <- function( old, new, burnin = 1000, n = 10000, core = 12 ) {
                 mu[[ k ]] %*% t( mu[[ k ]] )
             sigma[[ k ]] <- riwish( v, scl )
         }
+        det_sigma <- vector( "numeric", 4 )
+        inv_sigma <-matrix( 0, 16, 4 )
+        sigma_mu <- matrix( 0, 4, 4 )
+        q_sigma <- vector( "numeric", 4 )
+        for( l in 1 : 4 ) {
+            det_sigma[ l ] <- det( sigma[[ l ]] )
+            inv_sigma[ , l ] <- solve( sigma[[ l ]] )
+            sigma_mu[ , l ] <- solve( sigma[[ l ]] ) %*% mu[[ l ]]
+            q_sigma[ l ] <- t( mu[[ l ]] ) %*% solve( sigma[[ l ]] ) %*%
+                mu[[ l ]]
+        }
         # update x_star
         for( k in 1 : 8 ) {
-            det_sigma <- vector( "numeric", 4 )
-            inv_sigma <-matrix( 0, 16, 4 )
-            sigma_mu <- matrix( 0, 4, 4 )
-            q_sigma <- vector( "numeric", 4 )
-            for( l in 1 : 4 ) {
-                det_sigma[ l ] <- det( sigma[[ l ]] )
-                inv_sigma[ , l ] <- solve( sigma[[ l ]] )
-                sigma_mu[ , l ] <- solve( sigma[[ l ]] ) %*% mu[[ l ]]
-                q_sigma[ l ] <- t( mu[[ l ]] ) %*% solve( sigma[[ l ]] ) %*%
-                    mu[[ l ]]
-            }
-
             pmfLabel( alpha_star, beta_star, gamma_star,
                       det_sigma, inv_sigma, sigma_mu,
-                      q_sigma, neighbor_label, sub_modality_mat, prob, 12 )
+                      q_sigma, new[[ k ]]$neighbor_label,
+                      new[[ k ]]$sub_modality_mat,
+                      new[[ k ]]$prob, core )
+            updatePred( new, k )
+            len_type <- 4
+            sum_cross_y <- vector( "list", len_type )
+            sum_y <- vector( "list", len_type )
+            n_type <- tabulate( new[[ k ]]$pred_seg, nbins = len_type )
+            for( l in 1 : len_type ) {
+                index <- new[[ k ]]$pred_seg == l
+                y <- new[[ k ]]$sub_modality_mat[ , index ]
+                sum_cross_y[[ l ]] <- tcrossprod( y )
+                sum_y[[ l ]] <- rowSums( y )
+            }
+            new[[ k ]]$sum_cross_y <- sum_cross_y
+            new[[ k ]]$sum_y <- sum_y
+            new[[ k ]]$n_type <- n_type
+            if( k < 8 ) {
+                next_seq <- k + 1
+                fillNbrlabel( new$cube, new[[ next_seq ]]$neighbor_index,
+                              new[[ next_seq ]]$neighbor_label, core )
+            }
         }
-
-
-
-
+        for( k in 1 : 8 ) {
+            fillNbrlabel( new$cube, new[[ k ]]$neighbor_index,
+                          new[[ k ]]$neighbor_label, core )
+        }
     }
-
+    list( old = old, new = new, alpha_star_trace = alpha_star_trace,
+          beta_star_trace = beta_star_trace,
+          gamma_star_trace = gamma_star_trace )
 }
--- R/updatePred.R
+++ R/updatePred.R
@@ -1,10 +1,12 @@
 updatePred <- function( new_patient, i ) {
+    len_type <- 4
     # Sample the labels
-    new_pred <- apply( new_patient[[ i ]]$prob, 2, sample, x = 1 : 4, size = 1,
+    new_pred <- apply( new_patient[[ i ]]$prob, 2, sample, x = 1 : len_type,
+                       size = 1,
                        replace = FALSE  )
     storage.mode( new_pred ) <- "numeric"
     .Call( "updatePred", new_patient[[ i ]]$pred_seg, new_pred )
-    .Call( "updateCube", new_patient[[ 9 ]], new_patient[[ i ]]$sub_vec_index,
+    .Call( "updateCube", new_patient$cube, new_patient[[ i ]]$sub_vec_local,
            new_pred )
     .Call( "updateCount", new_patient[[ i ]]$count, new_pred )
 }
